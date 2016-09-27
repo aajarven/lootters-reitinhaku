7beaeaf6dc820e5f5e6645cc7e7ad89a3576493b
aajarven <anni.jarvenpaa@gmail.com>
2016-09-28 01:25:29 +0300

    uuden edeltäjänasettamistoiminnallisuuden testaus

--- reitinhaku/src/test/java/fi/antonbury/reitinhaku/haku/NodeTest.java
+++ reitinhaku/src/test/java/fi/antonbury/reitinhaku/haku/NodeTest.java
@@ -17,25 +17,25 @@ import static org.junit.Assert.*;
  * @author Anni Järvenpää
  */
 public class NodeTest {
-    
+
     Node n;
-    
+
     public NodeTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
-        n = new Node(3,5);
+        n = new Node(3, 5);
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -63,7 +63,6 @@ public class NodeTest {
         assertTrue("Edeltäjän asettaminen toimii väärin", n.getEdeltaja() == edeltaja);
     }
 
-
     /**
      * Test of getX method, of class Node.
      */
@@ -90,11 +89,11 @@ public class NodeTest {
         Node edeltaja = new Node(3, 6);
         edeltaja.nollaaG();
         n.yritaAsettajaaEdeltaja(edeltaja);
-        assertTrue("Kun nodella on yksi edeltäjä, getG-metodin pitäisi palauttaa 1", n.getG()==1);
+        assertTrue("Kun nodella on yksi edeltäjä, getG-metodin pitäisi palauttaa 1", n.getG() == 1);
         Node n2 = new Node(4, 6);
         n2.yritaAsettajaaEdeltaja(n);
         assertTrue("Kun nodella on yksi edeltäjä, jolla on edletäjä, getG-metodin"
-                + " pitäisi palauttaa 2", n2.getG()==2);
+                + " pitäisi palauttaa 2", n2.getG() == 2);
     }
 
     /**
@@ -111,5 +110,41 @@ public class NodeTest {
         n2.yritaAsettajaaEdeltaja(n);
         assertEquals(12, n2.f(maali), 0.001);
     }
-    
+
+    /**
+     * Test of yritaAsettajaaEdeltaja method, of class Node
+     */
+    @Test
+    public void testYritaAsettajaaEdeltaja() {
+        Node alku = new Node(0, 0);
+        alku.nollaaG();
+        Node va = new Node(0, 1); // vasen ala
+        Node oy = new Node(1, 0); // oikea ylä
+        Node oa = new Node(1, 1); // oikea ala
+        
+        assertTrue("Noden, jolla ei vielä ole edeltäjää, pitäisi suostua"
+                + "ottamaan alkunode edeltäjäkseen",
+                va.yritaAsettajaaEdeltaja(alku));
+        assertTrue("Noden, jolla ei vielä ole edeltäjää, pitäisi suostua"
+                + "ottamaan alkunode edeltäjäkseen",
+                oy.yritaAsettajaaEdeltaja(alku));
+        assertTrue("Noden, jolla ei vielä ole edeltäjää, pitäisi suostua "
+                + "ottamaan node jolla on edeltäjäkseen", 
+                oa.yritaAsettajaaEdeltaja(va));
+        assertFalse("Noden, jolla on jo parempi edeltäjä, ei pitäisi vaihtaa"
+                + " huonompaan", oy.yritaAsettajaaEdeltaja(oa));
+        
+        alku = new Node(0, 0);
+        alku.nollaaG();
+        va = new Node(0, 1); // vasen ala
+        oy = new Node(1, 0); // oikea ylä
+        oa = new Node(1, 1); // oikea ala
+        
+        va.yritaAsettajaaEdeltaja(alku);
+        oy.yritaAsettajaaEdeltaja(alku);
+        oa.yritaAsettajaaEdeltaja(va);
+        assertFalse("Noden ei pitäisi vaihtaa edeltäjää toiseen yhtä hyvään",
+                oa.yritaAsettajaaEdeltaja(oy));
+    }
+
 }
