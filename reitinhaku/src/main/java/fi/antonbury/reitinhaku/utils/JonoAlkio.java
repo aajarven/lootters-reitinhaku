31c776450497ecfb80826ed47cfde32ffa9bc3a0
aajarven <anni.jarvenpaa@gmail.com>
2016-09-28 00:52:58 +0300

    Omassa priorityqueue-toteutuksessa käytettävä alkioluokka

---
+++ reitinhaku/src/main/java/fi/antonbury/reitinhaku/utils/JonoAlkio.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.antonbury.reitinhaku.utils;
+
+import fi.antonbury.reitinhaku.haku.Node;
+
+/**
+ *
+ * @author anni
+ */
+public class JonoAlkio {
+    
+    private Node node;
+    private JonoAlkio seuraaja;
+
+    public JonoAlkio(Node node) {
+        this.node = node;
+        this.seuraaja = null;
+    }
+
+    public JonoAlkio getSeuraaja() {
+        return seuraaja;
+    }
+
+    public void setSeuraaja(JonoAlkio seuraaja) {
+        this.seuraaja = seuraaja;
+    }
+
+    public Node getNode() {
+        return node;
+    }
+    
+    public double kustannus(Node maali){
+        return node.f(maali);
+    }
+    
+    
+}
