514e1aa3595e580e5353610cf668b8386163c7dd
aajarven <anni.jarvenpaa@gmail.com>
2016-10-02 02:24:50 +0300

    kommenttihommat

--- reitinhaku/src/main/java/fi/antonbury/reitinhaku/utils/LuolaLukija.java
+++ reitinhaku/src/main/java/fi/antonbury/reitinhaku/utils/LuolaLukija.java
@@ -17,22 +17,22 @@ import java.util.logging.Logger;
  * @author anni
  */
 public class LuolaLukija {
-    
-    public static boolean[][] lueLuola(String tiedostonimi) throws IOException{
-        /**
-         * Lukee annetussa polussa olevasta tekstitiedostosta luolan. Luola 
-         * tallennetaan välilyönneillä ja rivinvaihdoilla erotettuna taulukkona
-         * ykkösiä ja nollia, joista ykköset edustavat ruutuja, joihin voi
-         * kävellä ja nollat ruutuja, joihin ei voi.
-         * 
-         * Tiedoston ensimmäisen rivin tulee sisältää kaksi välilyönnillä
-         * erotettua kokonaislukua, jotka kertovat luolan leveyden ja korkeuden.
-         * Risuaitamerkillä alkavia rivejä ei käsitellä.
-         * 
-         * @param tiedostonimi polku luoladatan sisältävään tiedostoon
-         * @return boolean-taulukko, jossa true edustaa kuljettavissa olevia ruutuja
-         */
-        
+
+    /**
+     * Lukee annetussa polussa olevasta tekstitiedostosta luolan. Luola
+     * tallennetaan välilyönneillä ja rivinvaihdoilla erotettuna taulukkona
+     * ykkösiä ja nollia, joista ykköset edustavat ruutuja, joihin voi kävellä
+     * ja nollat ruutuja, joihin ei voi.
+     *
+     * Tiedoston ensimmäisen rivin tulee sisältää kaksi välilyönnillä erotettua
+     * kokonaislukua, jotka kertovat luolan leveyden ja korkeuden.
+     * Risuaitamerkillä alkavia rivejä ei käsitellä.
+     *
+     * @param tiedostonimi polku luoladatan sisältävään tiedostoon
+     * @return boolean-taulukko, jossa true edustaa kuljettavissa olevia ruutuja
+     */
+    public static boolean[][] lueLuola(String tiedostonimi) throws IOException {
+
         Scanner lukija = null;
         lukija = new Scanner(new File(tiedostonimi));
 
@@ -40,31 +40,39 @@ public class LuolaLukija {
         taytaLuola(luola, lukija);
 
         return luola;
-        
+
     }
 
+    /**
+     * Generoi annetun scannerin lukemaa tiedostoa vastaavan boolean-taulukon
+     *
+     * @param luola kaksiulotteinen boolean-taulukko, jossa true vastaa
+     * kuljettavaa nodea ja false kulkukelvotonta
+     * @param lukija haluttua tiedostoa
+     * @throws IOException
+     */
     private static void taytaLuola(boolean[][] luola, Scanner lukija) throws IOException {
         int sarakkeet = luola[0].length;
         int riviNro = 0;
 
-        while(lukija.hasNextLine()){
+        while (lukija.hasNextLine()) {
             String rivi = lukija.nextLine();
-            
-            if (!onDatarivi(rivi)){
+
+            if (!onDatarivi(rivi)) {
                 continue;
             }
-            
+
             String[] luvut = rivi.split(" ");
-            if (luvut.length != sarakkeet){
+            if (luvut.length != sarakkeet) {
                 throw new IOException("Rivillä '" + rivi + "' oli odottamaton "
                         + "määrä lukuja (havaittu " + luvut.length
                         + ", odotettiin " + sarakkeet + "). Suoritus lopetetaan.");
             }
-            
-            for (int i = 0; i < luvut.length; i++){
+
+            for (int i = 0; i < luvut.length; i++) {
                 try {
                     int luku = Integer.parseInt(luvut[i]);
-                    if (luku == 0){
+                    if (luku == 0) {
                         luola[riviNro][i] = false;
                     } else {
                         luola[riviNro][i] = true;
@@ -73,32 +81,32 @@ public class LuolaLukija {
                     System.err.println("Luolatiedoston rivi '" + rivi
                             + "' sisälsi arvon jota ei voinut parsia "
                             + "kokonaisluvuksi.");
-                    throw(e);
+                    throw (e);
                 }
             }
-            
+
             riviNro++;
         }
     }
-    
-    private static boolean[][] alustaLuolataulukko(Scanner lukija){
-        /**
-         * Luo luolatiedoston ensimmäisellä rivillä annettua luolan kokoa
-         * vastaavan boolean-arrayn.
-         * 
-         * @param lukija Scanner, joka lukee 
-         */
+
+    /**
+     * Luo tiedoston alussa speksatun kokoisen tyhjän taulukon
+     *
+     * @param lukija scanner halutun tiedoston lukemiseen
+     * @return tyhjä kaksiulotteinen boolean-taulukko
+     */
+    private static boolean[][] alustaLuolataulukko(Scanner lukija) {
         String ekarivi;
-        
-        do{
+
+        do {
             ekarivi = lukija.nextLine();
-        } while(!onDatarivi(ekarivi));
-        
+        } while (!onDatarivi(ekarivi));
+
         String[] luvut = ekarivi.split(" ");
         int rivit = 0;
         int sarakkeet = 0;
-        
-        try{
+
+        try {
             rivit = Integer.parseInt(luvut[0]);
             sarakkeet = Integer.parseInt(luvut[1]);
         } catch (NumberFormatException e) {
@@ -111,21 +119,20 @@ public class LuolaLukija {
                     + "1 0 0 1\n"
                     + "1 1 0 1\n"
                     + "0 1 1 1\n");
-            throw(e);
+            throw (e);
         }
-        
+
         return new boolean[rivit][sarakkeet];
     }
-    
-    private static boolean onDatarivi(String rivi){
-        /**
-         * Palauttaa false jos rivi on tyhjä tai alkaa risuaitamerkillä (#),
-         * muuten true.
-         * 
-         * @param rivi tutkittava rivi
-         */
-        
+
+    /**
+     * Palauttaa false jos rivi on tyhjä tai alkaa risuaitamerkillä (#), muuten
+     * true.
+     *
+     * @param rivi tutkittava rivi
+     */
+    private static boolean onDatarivi(String rivi) {
         return !(rivi.length() == 0 || rivi.charAt(0) == '#');
     }
-    
+
 }
