6b32b63c92cc84400285c78fc447e00a191b27bf
aajarven <anni.jarvenpaa@gmail.com>
2016-10-03 22:28:20 +0300

    katselmoinnin hashset-idea käyttöön

--- reitinhaku/src/main/java/fi/antonbury/reitinhaku/haku/AStar.java
+++ reitinhaku/src/main/java/fi/antonbury/reitinhaku/haku/AStar.java
@@ -5,13 +5,12 @@
  */
 package fi.antonbury.reitinhaku.haku;
 
-import fi.antonbury.reitinhaku.utils.NodeGeneraattori;
 import fi.antonbury.reitinhaku.utils.PolkuGeneraattori;
 import fi.antonbury.reitinhaku.utils.Prioriteettijono;
-import java.util.HashSet;
 
 /**
  * A*-haku
+ *
  * @author anni
  */
 public class AStar {
@@ -20,7 +19,7 @@ public class AStar {
      * Etsii lyhyimmän polun annetusta lähtönodesta annettuun maalinodeen.
      * Mikäli polku on olemassa, palauttaa polun nodet taulukossa ensimmäisestä
      * viimeiseen. Mikäli polkua ei ole olemassa, palauttaa null.
-     * 
+     *
      * @param lahto lähtönode
      * @param maali maalinode
      * @return lähdön ja maalin välisen lyhyimmän polun nodet
@@ -28,40 +27,35 @@ public class AStar {
     public static Node[] search(Node lahto, Node maali) {
 
         Prioriteettijono openSet = new Prioriteettijono(maali);
-        HashSet<Node> closedSet = new HashSet<Node>();
 
         openSet.lisaaNode(lahto);
 
         while (!openSet.onTyhja()) {
             Node kasiteltava = openSet.poimiNode();
-            if (kasiteltava == maali){
+            if (kasiteltava == maali) {
                 return PolkuGeneraattori.generoiPolku(kasiteltava);
             }
-            
-            closedSet.add(kasiteltava);
-            
-            kasitteleNaapurit(openSet, closedSet, kasiteltava);
-            
+
+            kasitteleNaapurit(openSet, kasiteltava);
+
         }
-        
+
         return null;
     }
 
     /**
      * Lisää annetun noden kaikki naapurit annettuun NodeJonoon jos niitä ei jo
-     * löydy closedSetistä. Samalla naapurien g-arvo ja edeltävä solmu 
+     * löydy closedSetistä. Samalla naapurien g-arvo ja edeltävä solmu
      * päivitetään tarvittaessa.
      *
      * @param openSet NodeJono, johon naapurit lisätään
      * @param closedSet Set, josta löytyviä naapureita ei lisätä
      * @param lisattava Node, jonka naapurit käsitellään
      */
-    private static void kasitteleNaapurit(Prioriteettijono openSet, HashSet<Node> closedSet, Node lisattava) {
+    private static void kasitteleNaapurit(Prioriteettijono openSet, Node lisattava) {
         for (Node n : lisattava.getNaapurit()) {
-            if (!closedSet.contains(n)) {
-                if (n.yritaAsettajaaEdeltaja(lisattava)){
-                    openSet.lisaaNode(n);
-                }
+            if (n.yritaAsettajaaEdeltaja(lisattava)) {
+                openSet.lisaaNode(n);
             }
         }
     }
